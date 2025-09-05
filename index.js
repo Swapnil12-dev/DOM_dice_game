53212005a6bf805ef12e84fdd2221a7c645a5bbd
Swapnil12-dev <swapnil.ajabe1212@gmail.com>
2025-09-05 20:39:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var random1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + random1 + ".png";
+var randomImageSource = "./images/" + randomDiceImage;
+
+document.querySelector("img")[0].setAttribute("src", randomImageSource);
+
+var random2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + random2 + ".png";
+var randomImageSource2 = "./images/" + randomDiceImage2;
+
+document.querySelector("img")[1].setAttribute("src", randomImageSource2);
+
+alert("wefw");
+
+
+
+
+
+
