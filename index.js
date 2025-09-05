6e5c97233e7db652a5d5c5f9424205c0378fc8ce
Swapnil12-dev <swapnil.ajabe1212@gmail.com>
2025-09-05 20:47:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ var random1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage = "dice" + random1 + ".png";
 var randomImageSource = "./images/" + randomDiceImage;
 
-document.querySelector("img")[0].setAttribute("src", randomImageSource);
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 var random2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage2 = "dice" + random2 + ".png";
 var randomImageSource2 = "./images/" + randomDiceImage2;
 
-document.querySelector("img")[1].setAttribute("src", randomImageSource2);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
+
+if (random1 > random2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (random2 > random1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "🎲 It's a Draw!";
+}
 
 
 
